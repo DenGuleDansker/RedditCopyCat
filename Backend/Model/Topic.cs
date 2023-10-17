a71518342b49f09f804a3f024a4e894486857747
Marcus <eaamarbb@students.eaaa.dk>
2023-10-17 13:23:34 +0200

    små ændringer

--- Backend/Model/Topic.cs
+++ Backend/Model/Topic.cs
@@ -8,21 +8,21 @@ namespace Model
 
         }
 
-        public Topic(string title, string description, string user, DateTime date, int votes)
+        public Topic(string title, string description, string user, int votes)
         {
             this.Title = title;
             this.Description = description;
             this.User = user;
-            this.Date = date;
+            this.Date = DateTime.Now;
             this.Votes = votes;
         }
 
         public long TopicID { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public string? Description { get; set; }
-        public string User { get; set; }
-        public DateTime Date { get; set; }
-        public int Votes { get; set; }
+        public string? User { get; set; }
+        public DateTime? Date { get; set; }
+        public int? Votes { get; set; }
 
         public List<Comment> Comment { get; set; }
 
