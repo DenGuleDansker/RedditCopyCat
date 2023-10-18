dbd9759fd1b3210c1cdb66e0c6f3da717a1d6979
FynboNohr69420 <117977003+FynboNohr69420@users.noreply.github.com>
2023-10-18 12:17:28 +0200

    Frontend added

---
+++ Shared1/Model/Topic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Model
+{
+    public class Topic
+    {
+        public Topic()
+        {
+
+        }
+
+        public Topic(string title, string description, string user)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.User = user;
+            this.Date = DateTime.Now;
+            this.Votes = 0;
+        }
+
+      
+
+        public long TopicID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+        public int Votes { get; set; }
+
+        public List<Comment> Comment { get; set; }
+
+    }
+}
+
---
+++ Shared1/Model/TopicContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class TopicContext : DbContext
+    {
+        public DbSet<Topic> Topics { get; set; } // Assuming 'Topic' is your entity type
+        public DbSet<Comment> Comment { get; set; } // Assuming 'Topic' is your entity type
+
+        //// Bruges måske senere
+        public string DbPath { get; }
+
+        public TopicContext(DbContextOptions<TopicContext> options)
+             : base(options)
+        {
+            // Den her er tom. Men ": base(options)" sikre at constructor
+            // på DbContext super-klassen bliver kaldt.
+        }
+    }
+}
