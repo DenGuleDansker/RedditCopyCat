175e657d39ebb09c576404887db7c60aaf9f17f0
FynboNohr69420 <kaspernohr@live.dk>
2023-10-28 18:04:40 +0200

    Another attempt at a manual merge

---
+++ Fontnhy/Service/ApiService.cs
@@ -0,0 +1,166 @@
+//THIS CODE IS FROM HIS EXAMPLE
+using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Model;
+
+namespace backend.Data;
+
+public class ApiService
+{
+    private readonly HttpClient http;
+    private readonly IConfiguration configuration;
+    private readonly string baseAPI = "";
+
+    public ApiService(HttpClient http, IConfiguration configuration)
+    {
+        this.http = http;
+        this.configuration = configuration;
+        this.baseAPI = configuration["base_api"];
+    }
+
+    public async Task<Topic[]> GetTopics()
+    {
+        string url = $"{baseAPI}topics";
+        return await http.GetFromJsonAsync<Topic[]>(url);
+    }
+
+    public async Task<Topic> GetTopic(int id)
+    {
+        string url = $"{baseAPI}topic/{id}";
+        return await http.GetFromJsonAsync<Topic>(url);
+    }
+
+    public async Task<Comment[]> GetComments(int topicid)
+    {
+        string url = $"{baseAPI}{topicid}/comments";
+        return await http.GetFromJsonAsync<Comment[]>(url);
+    }
+
+    public async Task<Topic> CreateTopic(string title, string description, string user, DateTime date, int votes)
+    {
+        string url = $"{baseAPI}topics";
+
+        // Post JSON to API, save the HttpResponseMessage
+        HttpResponseMessage msg = await http.PostAsJsonAsync(url, new { title, description, user, date, votes });
+
+        // Get the JSON string from the response
+        string json = msg.Content.ReadAsStringAsync().Result;
+
+        // Deserialize the JSON string to a Comment object
+        Topic? newTopic = JsonSerializer.Deserialize<Topic>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true // Ignore case when matching JSON properties to C# properties 
+        });
+
+        // Return the new comment 
+        return newTopic;
+    }
+
+
+    public async Task<Comment> CreateComment(string description, int topicid, string user)
+    {
+        string url = $"{baseAPI}comment";
+
+        // Post JSON to API, save the HttpResponseMessage
+        HttpResponseMessage msg = await http.PostAsJsonAsync(url, new { description, user, topicid});
+
+        // Get the JSON string from the response
+        string cmtjson = msg.Content.ReadAsStringAsync().Result;
+
+        Console.WriteLine(cmtjson);
+
+        // Deserialize the JSON string to a Comment object
+        Comment? newComment = JsonSerializer.Deserialize<Comment>(cmtjson, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true // Ignore case when matching JSON properties to C# properties 
+        });
+
+        // Return the new comment 
+        return newComment;
+    }
+
+    //Topic upvote-downvote
+    public async Task<Topic> UpvoteTopic(int topicid)
+    {
+        string url = $"{baseAPI}topic/{topicid}/upvote/";
+
+        // Post JSON to API, save the HttpResponseMessage
+        HttpResponseMessage msg = await http.PutAsJsonAsync(url, "");
+
+        // Get the JSON string from the response
+        string json = msg.Content.ReadAsStringAsync().Result;
+
+        // Deserialize the JSON string to a Post object
+        Topic? updatedTopic = JsonSerializer.Deserialize<Topic>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true // Ignore case when matching JSON properties to C# properties
+        });
+
+        // Return the updated post (vote increased)
+        return updatedTopic;
+    }
+
+    public async Task<Topic> DownvoteTopic(int topicid)
+    {
+        string url = $"{baseAPI}topic/{topicid}/downvote/";
+
+        // Post JSON to API, save the HttpResponseMessage
+        HttpResponseMessage msg = await http.PutAsJsonAsync(url, "");
+
+        // Get the JSON string from the response
+        string json = msg.Content.ReadAsStringAsync().Result;
+
+        // Deserialize the JSON string to a Post object
+        Topic? updatedDownTopic = JsonSerializer.Deserialize<Topic>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true // Ignore case when matching JSON properties to C# properties
+        });
+        // Return the updated post (vote increased)
+        return updatedDownTopic;
+    }
+
+    //Comment upvote-downvote
+    public async Task<Comment> UpvoteComment(int topicid, int commentid)
+    {
+        string url = $"{baseAPI}topic/{topicid}/comment/{commentid}/upvote/";
+
+        // Post JSON to API, save the HttpResponseMessage
+        HttpResponseMessage msg = await http.PutAsJsonAsync(url, "");
+
+        // Get the JSON string from the response
+        string json = msg.Content.ReadAsStringAsync().Result;
+
+        // Deserialize the JSON string to a Post object
+        Comment? updatedComment = JsonSerializer.Deserialize<Comment>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true // Ignore case when matching JSON properties to C# properties
+        });
+
+        // Return the updated post (vote increased)
+        return updatedComment;
+    }
+
+    public async Task<Comment> DownvoteComment(int topicid, int commentid)
+    {
+        string url = $"{baseAPI}topic/{topicid}/comment/{commentid}/downvote/";
+
+        // Post JSON to API, save the HttpResponseMessage
+        HttpResponseMessage msg = await http.PutAsJsonAsync(url, "");
+
+        // Get the JSON string from the response
+        string json = msg.Content.ReadAsStringAsync().Result;
+
+        // Deserialize the JSON string to a Post object
+        Comment? updateDownComment = JsonSerializer.Deserialize<Comment>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true // Ignore case when matching JSON properties to C# properties
+        });
+
+        // Return the updated post (vote increased)
+        return updateDownComment;
+    }
+
+    public record CommentDTO(string description, string user, DateTime date, int votes, long topicid);
+    public record TopicDTO(string title, string description, string user, DateTime date, int votes);
+}
