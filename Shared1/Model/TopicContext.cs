4055a1a320a1b670ffb22924d1eba312e3f12410
FynboNohr69420 <kaspernohr@live.dk>
2023-10-28 17:49:49 +0200

    test commit, manual merge of gg into main

---
+++ Shared1/Model/TopicContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class TopicContext : DbContext
+    {
+        public DbSet<Topic> Topics { get; set; } // Assuming 'Topic' is your entity type
+        public DbSet<Comment> Comment { get; set; } // Assuming 'Topic' is your entity type
+
+        //// Bruges måske senere
+        public string DbPath { get; }
+
+        public TopicContext(DbContextOptions<TopicContext> options)
+             : base(options)
+        {
+            // Den her er tom. Men ": base(options)" sikre at constructor
+            // på DbContext super-klassen bliver kaldt.
+        }
+    }
+}
